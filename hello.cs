4f7ea994b6a217249a2b1084276745844c4daa74
enescodetr <enesyilmaz.code@gmail.com>
2025-04-06 05:54:55 +0300

    hello.cs

--- hello.cs
+++ hello.cs
@@ -2,7 +2,7 @@ public class hello{
 public static void main(String[] args){
 
 Console.WriteLine("Merhaba!");
-Console.WirteLine("Bu proje C# konsol aplikasyonu ile kodlandı.")
+Console.WirteLine("Bu proje C# form aplikasyonu ile kodlandı.")
   
 }
 }
